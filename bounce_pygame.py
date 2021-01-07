bd7dc274275f8e3111a6497c3358c6cb314cdece
Darth Pjotr <DarthPjotr@users.noreply.github.com>
2021-01-07 09:49:57 +0100

    ""

--- bounce_pygame.py
+++ bounce_pygame.py
@@ -4,14 +4,7 @@ import random
 import time
 import pygame
 from gas import *
-
-def getcolors(steps):
-    colors = []
-    for i in range(0,255, round(255/steps)):
-        color = (255-i,0,i)
-        colors.append(color)
-    return colors
-       
+     
 
 def paint():
     """
@@ -24,7 +17,6 @@ def paint():
     screen = pygame.display.set_mode(size[:2], pygame.RESIZABLE)
 
     pygame.display.set_caption("Bouncing Balls")
-    #colors = getcolors(NBALLS)
 
     # Loop until the user clicks the close button.
     done = False
@@ -34,19 +26,8 @@ def paint():
     clock = pygame.time.Clock()
 
     box_sizes = size[:DIMENSIONS]
-    #box_sizes.append(100)
-    #box_sizes.append(SCREEN_DEPTH)
-    #print(box_sizes)
+
     box = Box(box_sizes)
-    # ball in border
-    #box.add_particle(MASS, RADIUS, [-5,size[1]/2], [-1,0])
-    # ball overlap
-    #box.add_particle(MASS, RADIUS, [size[0]/2,size[1]/2], [1,0])
-    #box.add_particle(MASS, RADIUS, [size[0]/2+RADIUS/2,size[1]/2], [-1,1])
-    for i in range(NBALLS):
-        #mass = MASS*random.random()+1
-        #radius = mass*5
-        box.add_particle(mass=MASS, radius=RADIUS)
 
     # -------- Main Program Loop -----------
     while not done:
@@ -57,9 +38,8 @@ def paint():
             elif event.type == pygame.KEYDOWN:
                 # Space bar! Spawn a new ball.
                 if event.key == pygame.K_SPACE:
-                    #mass = MASS*random.random()+1
-                    #radius = mass*5
-                    box.add_particle(mass=MASS, radius=RADIUS)
+                    mass = random.randrange(5, 50)
+                    box.add_particle(mass=mass, radius=mass)
                 elif event.key == pygame.K_p:
                     pause = not pause
                 elif event.key == pygame.K_q:
@@ -71,30 +51,14 @@ def paint():
 
         if not pause:
             box.go()
-
-            #print(sum([ball.energy() for ball in box.particles]))
-        
+       
             # --- Drawing
             # Set the screen background
             screen.fill(BLACK)
 
             # Draw the balls
             for i, ball in enumerate(box.particles):
-                if box.dimensions > 2:
-                    radius = ball.radius*(ball.position[-1]/SCREEN_DEPTH)
-                else:
-                    radius = ball. radius
-                color = WHITE
-                if i == 0:
-                    color = RED
-                elif i == box.dimensions-1:
-                    color = GREEN
-                elif False:
-                    idx = round(ball.speed.dot(ball.speed)) % len(colors)
-                    #print(idx, ball.speed.dot(ball.speed))
-                    color = colors[idx]
-
-                pygame.draw.circle(screen, color, ball.position[:2], radius)
+                pygame.draw.circle(screen, ball.color, ball.position[:2], ball.radius)
                 pygame.draw.line(screen, BLUE, ball.position[:2], (ball.position + 5*ball.speed)[:2], width=2)
 
         # --- Wrap-up
@@ -107,30 +71,6 @@ def paint():
 
     # Close everything down
     pygame.quit()
-
-def main():
-    
-    box_sizes = [50,100]
-    #box_sizes = [1,2]
-    box = Box(box_sizes)
-    # print(box.vertices)
-    print(box)
-    print(box.vertices)
-    for i, v in enumerate(box.vertices):
-    	print(i, v)
-    print(box.egdes)
-
-    box.add_particle()
-    box.add_particle()
-    for p in box.particles:
-        print(p)
-
-    try:
-        while True:
-            box.go()
-            time.sleep(0.3)
-    except KeyboardInterrupt:
-        pass
 
 if __name__ == "__main__":
     random.seed()
--- bounce_tk.py
+++ bounce_tk.py
@@ -1,33 +1,57 @@
 from tkinter import *
 from random import randint
+from gas import *
+import numpy
+
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
 
 class Ball:
-    def __init__(self, canvas, x1, y1, x2, y2):
+    def __init__(self, canvas, box, ball):
+        R = ball.radius
+        x1 = round(ball.position[0] - R)
+        x2 = round(ball.position[0] + R)
+        y1 = round(ball.position[1] - R)
+        y2 = round(ball.position[1] + R)
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
+        self.box = box
+        self.ball = ball
         self.canvas = canvas
-        self.ball = canvas.create_oval(self.x1, self.y1, self.x2, self.y2, fill="red")
+        self.object = canvas.create_oval(self.x1, self.y1, self.x2, self.y2, fill="red")
 
     def move_ball(self):
-        deltax = randint(0,5)
-        deltay = randint(0,5)
-        self.canvas.move(self.ball, deltax, deltay)
+        old = numpy.array(self.ball.position[:])
+        self.ball.move()
+        self.ball.bounce(self.box)
+        for b2 in self.box.particles:
+            self.ball.collide(b2)
+        delta = self.ball.position - old
+        self.canvas.move(self.object, delta[0], delta[1])
         self.canvas.after(60, self.move_ball)
 
-# initialize root Window and canvas
-root = Tk()
-root.title("Balls")
-root.resizable(False,False)
-canvas = Canvas(root, width = 300, height = 300)
-canvas.pack()
+def main():
+    # initialize root Window and canvas
+    root = Tk()
+    root.title("Balls")
+    root.resizable(False,False)
+    canvas = Canvas(root, width = SCREEN_WIDTH, height = SCREEN_HEIGHT)
+    canvas.pack()
 
-# create two ball objects and animate them
-ball1 = Ball(canvas, 10, 10, 30, 30)
-ball2 = Ball(canvas, 60, 60, 80, 80)
+    box = Box([SCREEN_WIDTH, SCREEN_HEIGHT])
+    for i in range(10):
+        mass = random.randrange(5, 50)
+        box.add_particle(mass, mass)
 
-ball1.move_ball()
-ball2.move_ball()
+    balls = []
+    for ball in box.particles:
+        balls.append(Ball(canvas, box, ball))
 
-root.mainloop()
+    for ball in balls:
+        ball.move_ball()
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
